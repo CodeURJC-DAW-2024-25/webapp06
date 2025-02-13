22ef758459995dac804654116e1a1ed8b22b1c61
teijeiro7 <teijeiroparga2004@gmail.com>
2025-02-13 12:15:13 +0100

    remove unnnecesary folders and files

--- webapp6/src/main/java/es/codeurjc/global_mart/controller/MainController.java
+++ webapp6/src/main/java/es/codeurjc/global_mart/controller/MainController.java
@@ -6,48 +6,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    
 
-//Functions to redirect to the different pages of the application
-	//Initial page (index.html)
-    @GetMapping("/")
+	// Functions to redirect to the different pages of the application
+	// Initial page (index.html)
+	@GetMapping("/")
 	public String greeting(Model model) {
 		return "index";
 	}
-	//Redirection to the initial page
+
+	// Redirection to the initial page
 	@GetMapping("/redir")
 	public String redir(Model model) {
 		return "redirect:/";
 	}
-	//Redirection to the login page
+
+	// Redirection to the login page
 	@GetMapping("/choose_login_option")
 	public String choose_login(Model model) {
 		return "choose_login_option";
 	}
-	//Redirection to the register page
+
+	// Redirection to the register page
 	@GetMapping("/register")
 	public String register(Model model) {
 		return "register";
 	}
-	//Redirection to the login page
+
+	// Redirection to the login page
 	@GetMapping("/login")
 	public String login(Model model) {
 		return "login";
 	}
-	//Redirection to the about us page
-	@GetMapping("/about_us")
+
+	// Redirection to the about us page
+	@GetMapping("/about")
 	public String about_us(Model model) {
 		return "about";
 	}
 
-
-
-
-
-//Redirection to see al prods (NEED UPDATE)
-	@GetMapping("/seeAllProds")
+	// Redirection to see al prods (NEED UPDATE)
+	@GetMapping("/products")
 	public String seeAllProds(Model model) {
 		return "products";
 	}
-
 }
