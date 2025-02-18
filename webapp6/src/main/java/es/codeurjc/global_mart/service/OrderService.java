1375596c64658e013361fa10fd02b6e1b24f170f
Juanma <juanmabm10@gmail.com>
2025-02-18 10:16:53 +0100

    actualización del order

--- webapp6/src/main/java/es/codeurjc/global_mart/service/OrderService.java
+++ webapp6/src/main/java/es/codeurjc/global_mart/service/OrderService.java
@@ -1,10 +1,12 @@
 package es.codeurjc.global_mart.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import es.codeurjc.global_mart.repository.OrderRepository;
 import es.codeurjc.global_mart.model.Order;
-
+// import es.codeurjc.global_mart.model.Product;
 
 @Service
 public class OrderService {
@@ -12,12 +14,23 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-    public Order createOrder(){
+    public Order createOrder() {
         Order order = new Order();
+        
+
         return orderRepository.save(order);
     }
+    
 
+    public List<Order> findAllOrders() {
+        return orderRepository.findAll();
+    }
 
+    public Order findOrderById(Long id) {
+        return orderRepository.findById(id).orElseThrow(() -> new RuntimeException("Order not found with id " + id));
+    }
+
+    
 
 
 }
