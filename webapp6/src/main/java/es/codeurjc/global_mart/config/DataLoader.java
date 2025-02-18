44075cee08d9ab5a522414ca5fa4000b53d65776
luis <luis@a.com>
2025-02-18 11:07:51 +0100

    fixed the dataloader

--- webapp6/src/main/java/es/codeurjc/global_mart/config/DataLoader.java
+++ webapp6/src/main/java/es/codeurjc/global_mart/config/DataLoader.java
@@ -24,23 +24,18 @@ public class DataLoader {
 
     @PostConstruct
     public void loadData() {
-<<<<<<< HEAD
-        productService.createProduct("Libros", "Producto1", "Amazon", 20.0, "Muy chulo", "String image");
+
+        productService.createProduct("Libros", "Producto1", "Amazon", 20.0, "Muy chulo", "https://preview.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=640&crop=smart&auto=webp&s=22ed6cc79cba3013b84967f32726d087e539b699");
         productService.createProduct("Electronica", "Producto2", "eBay", 30.0, "Muy útil", "String image");
         productService.createProduct("Electronica", "Producto3", "Walmart", 40.0, "Muy práctico", "String image");
         productService.createProduct("Deporte", "Producto4", "Decathlon", 50.0, "Muy resistente", "String image");
         productService.createProduct("Musica", "Producto5", "Zara", 60.0, "Muy elegante", "String image");
-=======
+
 
         userService.createUser("user1", "", "");
 
         reviewService.createReview("user1", "Muy bueno", 5);
 
-        productService.createProduct("Hogar", "Producto1", "Amazon", 20.0, "Muy chulo", "String image");
-        productService.createProduct("Electrónica", "Producto2", "eBay", 30.0, "Muy útil", "String image");
-        productService.createProduct("Jardinería", "Producto3", "Walmart", 40.0, "Muy práctico", "String image");
-        productService.createProduct("Deportes", "Producto4", "Decathlon", 50.0, "Muy resistente", "String image");
-        productService.createProduct("Moda", "Producto5", "Zara", 60.0, "Muy elegante", "String image");
->>>>>>> 67df06f2d27948ddcf1094ad78219d0f0ef0dd6f
+ 
     }
 }
