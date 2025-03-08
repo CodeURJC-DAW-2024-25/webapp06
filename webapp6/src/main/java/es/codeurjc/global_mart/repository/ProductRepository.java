c7a6f8454ad6801728c52c7af6b3ddc629b044d5
Alvaropin17 <alvaro17pc@gmail.com>
2025-03-08 12:30:46 +0100

    Ajax pageable WIP

--- webapp6/src/main/java/es/codeurjc/global_mart/repository/ProductRepository.java
+++ webapp6/src/main/java/es/codeurjc/global_mart/repository/ProductRepository.java
@@ -5,6 +5,8 @@ import es.codeurjc.global_mart.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -14,5 +16,14 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByNameContainingIgnoreCase(String name);
 
     List<Product> findByNameContainingIgnoreCaseAndType(String name, String type);
+
+
+
+    Page<Product> findByIsAcceptedTrue(Pageable pageable);
+    Page<Product> findByIsAcceptedTrueAndType(String type, Pageable pageable);
+    Page<Product> findByIsAcceptedTrueAndCompany(String company, Pageable pageable);
+    
+    
+    
 
 }
