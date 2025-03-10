26845739c7eee03f6398227934154b15482556fd
LuisAbril <luisabrilandreu@gmail.com>
2025-03-10 10:22:46 +0100

    working type loadMore ajax

--- webapp6/src/main/resources/static/js/loadMoreTypes.js
+++ webapp6/src/main/resources/static/js/loadMoreTypes.js
@@ -1,9 +1,9 @@
 let pageType = 0;
 
 
-async function loadMoreTypes() {
+async function loadMoreTypes(type) {
     pageType++;
-    let response = await fetch(`/moreProdsTypes?page=${pageType}`);
+    let response = await fetch(`/moreProdsTypes?page=${pageType}&type=${type}`);
     let data = await response.text();
     document.getElementById("productsContainer").innerHTML += data;
 
