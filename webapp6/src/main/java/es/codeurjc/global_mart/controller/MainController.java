d47a781da8aa82649ffd9d1751392bc0f9f8727a
teijeiro7 <teijeiroparga@outlook.com>
2025-03-10 10:41:17 +0100

    hola

--- webapp6/src/main/java/es/codeurjc/global_mart/controller/MainController.java
+++ webapp6/src/main/java/es/codeurjc/global_mart/controller/MainController.java
@@ -11,7 +11,11 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import es.codeurjc.global_mart.model.Product;
+import es.codeurjc.global_mart.model.Review;
 import es.codeurjc.global_mart.model.User;
 import es.codeurjc.global_mart.security.CSRFHandlerConfiguration;
 import es.codeurjc.global_mart.service.ProductService;
@@ -33,6 +37,21 @@ public class MainController {
 
 	MainController(CSRFHandlerConfiguration CSRFHandlerConfiguration) {
 		this.CSRFHandlerConfiguration = CSRFHandlerConfiguration;
+	}
+
+	private void addImageDataToProducts(List<Product> products) {
+		for (Product product : products) {
+			try {
+				Blob imageBlob = product.getImage();
+				if (imageBlob != null) {
+					byte[] bytes = imageBlob.getBytes(1, (int) imageBlob.length());
+					String imageBase64 = "data:image/jpeg;base64," + Base64.getEncoder().encodeToString(bytes);
+					product.setImageBase64(imageBase64); // Necesitas añadir este campo a la clase Product
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	// Functions to redirect to the different pages of the application
@@ -109,19 +128,57 @@ public class MainController {
 		return "user"; // Nombre del archivo HTML de la vista
 	}
 
-	private void addImageDataToProducts(List<Product> products) {
-		for (Product product : products) {
-			try {
-				Blob imageBlob = product.getImage();
-				if (imageBlob != null) {
-					byte[] bytes = imageBlob.getBytes(1, (int) imageBlob.length());
-					String imageBase64 = "data:image/jpeg;base64," + Base64.getEncoder().encodeToString(bytes);
-					product.setImageBase64(imageBase64); // Necesitas añadir este campo a la clase Product
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+	// Function to add review to a product
+	@PostMapping("/product/{id}/new_review")
+	public String postMethodName(@RequestParam int calification, @RequestParam String comment,
+			Authentication autentication, @PathVariable Long id) {
+
+		Optional<Product> product = productService.getProductById(id);
+		Review review = new Review();
+		review.setCalification(calification);
+		review.setComment(comment);
+		Object principal = autentication.getPrincipal();
+		if (principal instanceof OAuth2User oAuth2User) {
+			review.setUsername(oAuth2User.getAttribute("name"));
+		} else if (principal instanceof org.springframework.security.core.userdetails.User userDetails) {
+			review.setUsername(userDetails.getUsername());
 		}
+
+		if (product.isPresent()) {
+			product.get().addReview(review);
+			productService.addProduct(product.get());
+			return "redirect:/product/" + id;
+		} else {
+			return "redirect:/error";
+		}
+	}
+
+	public CSRFHandlerConfiguration getCSRFHandlerConfiguration() {
+		return CSRFHandlerConfiguration;
+	}
+
+	public ProductService getProductService() {
+		return productService;
+	}
+
+	public void setProductService(ProductService productService) {
+		this.productService = productService;
+	}
+
+	public UserService getUserService() {
+		return userService;
+	}
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+	public SearchController getSearchController() {
+		return searchController;
+	}
+
+	public void setSearchController(SearchController searchController) {
+		this.searchController = searchController;
 	}
 
 }
---
+++ webapp6/src/main/java/es/codeurjc/global_mart/controller/ReviewsController.java
@@ -0,0 +1,44 @@
+package es.codeurjc.global_mart.controller;
+
+import java.util.Optional;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import es.codeurjc.global_mart.model.Product;
+import es.codeurjc.global_mart.model.Review;
+import es.codeurjc.global_mart.service.ProductService;
+
+public class ReviewsController {
+
+    ProductService productService;
+
+    // Function to add review to a product
+    @PostMapping("/product/{id}/new_review")
+    public String postMethodName(@RequestParam int calification, @RequestParam String comment,
+            Authentication autentication, @PathVariable Long id) {
+
+        Optional<Product> product = productService.getProductById(id);
+        Review review = new Review();
+        review.setCalification(calification);
+        review.setComment(comment);
+        Object principal = autentication.getPrincipal();
+        if (principal instanceof OAuth2User oAuth2User) {
+            review.setUsername(oAuth2User.getAttribute("name"));
+        } else if (principal instanceof org.springframework.security.core.userdetails.User userDetails) {
+            review.setUsername(userDetails.getUsername());
+        }
+
+        if (product.isPresent()) {
+            product.get().addReview(review);
+            productService.addProduct(product.get());
+            return "redirect:/product/" + id;
+        } else {
+            return "redirect:/error";
+        }
+    }
+
+}
