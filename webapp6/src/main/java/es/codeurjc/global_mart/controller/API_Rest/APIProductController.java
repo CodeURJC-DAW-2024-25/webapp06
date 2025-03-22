2153f48cf89f2a91af04d5f217942577eb22855a
LuisAbril <luisabrilandreu@gmail.com>
2025-03-22 17:44:55 +0100

    APIPRoductController working

--- webapp6/src/main/java/es/codeurjc/global_mart/controller/API_Rest/APIProductController.java
+++ webapp6/src/main/java/es/codeurjc/global_mart/controller/API_Rest/APIProductController.java
@@ -1,11 +1,13 @@
 package es.codeurjc.global_mart.controller.API_Rest;
 
+import es.codeurjc.global_mart.dto.Product.ProductDTO;
 import es.codeurjc.global_mart.model.Product;
 import es.codeurjc.global_mart.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.method.P;
 import org.springframework.web.bind.annotation.*;
-
+import es.codeurjc.global_mart.dto.Product.ProductMapper;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,30 +18,38 @@ public class APIProductController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private ProductMapper productMapper;
+    
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
-        List<Product> products = productService.getAcceptedProducts();
-        return ResponseEntity.ok(products);
+        List<ProductDTO> products = productService.getAcceptedProducts();
+        List<Product> productsThrow = productMapper.toProducts(products);
+        return ResponseEntity.ok(productsThrow);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Long id) {
-        Optional<Product> product = productService.getProductById(id);
-        return product.map(ResponseEntity::ok)
-                      .orElseGet(() -> ResponseEntity.notFound().build());
+        Optional<ProductDTO> product = productService.getProductById(id);
+        if (product.isPresent()) {
+            return ResponseEntity.ok(productMapper.toProduct(product.get()));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        Product savedProduct = productService.addProduct(product);
-        return ResponseEntity.ok(savedProduct);
+    public ResponseEntity<Product> createProduct(@RequestBody Product productPassed) {
+        ProductDTO product = productMapper.toProductDTO(productPassed);
+        ProductDTO savedProduct = productService.addProduct(product);
+        return ResponseEntity.ok(productMapper.toProduct(savedProduct));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
-        Product updatedProduct = productService.updateProduct(id, product);
+        ProductDTO updatedProduct = productService.updateProduct(id, product);
         if (updatedProduct != null) {
-            return ResponseEntity.ok(updatedProduct);
+            return ResponseEntity.ok(productMapper.toProduct(updatedProduct));
         } else {
             return ResponseEntity.notFound().build();
         }
