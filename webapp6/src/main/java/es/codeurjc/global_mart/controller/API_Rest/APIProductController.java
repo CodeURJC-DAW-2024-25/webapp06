ad63bfad923c2fca60888a2befb2393309fc0aa1
teijeiro7 <teijeiroparga@outlook.com>
2025-03-24 20:39:12 +0100

    Enhance product creation and retrieval by adding image handling and updating method signatures

--- webapp6/src/main/java/es/codeurjc/global_mart/controller/API_Rest/APIProductController.java
+++ webapp6/src/main/java/es/codeurjc/global_mart/controller/API_Rest/APIProductController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.method.P;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,42 +24,43 @@ import java.util.Optional;
 @RequestMapping("/api/products")
 public class APIProductController {
 
-    @Autowired
-    private ProductService productService;
+	@Autowired
+	private ProductService productService;
 
-    @Autowired
-    private ProductMapper productMapper;
-    
-    @GetMapping
-    public ResponseEntity<List<ProductDTO>> getAllProducts() {
-        List<ProductDTO> products = productService.getAcceptedProducts();
-        return ResponseEntity.ok(products);
-    }
+	@Autowired
+	private ProductMapper productMapper;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
-        Optional<ProductDTO> product = productService.getProductById(id);
-        if (product.isPresent()) {
-            return ResponseEntity.ok(product.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+	@GetMapping
+	public ResponseEntity<List<ProductDTO>> getAllProducts() {
+		List<ProductDTO> products = productService.getAcceptedProducts();
+		return ResponseEntity.ok(products);
+	}
 
-    @PostMapping("/")
+	@GetMapping("/{id}")
+	public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
+		Optional<ProductDTO> product = productService.getProductById(id);
+		if (product.isPresent()) {
+			return ResponseEntity.ok(product.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PostMapping("/")
 	public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO) throws IOException {
 
-		productDTO = productService.createProduct(productDTO.type(), productDTO.name(), productDTO.company(), productDTO.price(),productDTO.description(),null,productDTO.stock(), productDTO.isAccepted());
-											
+		productDTO = productService.createProduct(productDTO.type(), productDTO.name(), productDTO.company(),
+				productDTO.price(), productDTO.description(), null, productDTO.stock(), productDTO.isAccepted(),
+				productDTO.reviews());
+
 		URI location = fromCurrentRequest().path("/{id}").buildAndExpand(productDTO.id()).toUri();
 
 		return ResponseEntity.created(location).body(productDTO);
 	}
 
-
-
 	@PutMapping("/{id}")
-	public ProductDTO updateProduct(@PathVariable long id, @RequestBody ProductDTO updatedProductDTO) throws SQLException {
+	public ProductDTO updateProduct(@PathVariable long id, @RequestBody ProductDTO updatedProductDTO)
+			throws SQLException {
 		Product updatedBookDTO = productMapper.toProduct(updatedProductDTO);
 		return productService.updateProduct(id, updatedBookDTO);
 	}
