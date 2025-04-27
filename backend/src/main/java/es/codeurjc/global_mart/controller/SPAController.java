7a6b66384f9833b0d1b2d146286020261b4338ec
Alvaropin17 <alvaro17pc@gmail.com>
2025-04-28 01:06:34 +0200

    2nd fix

--- backend/src/main/java/es/codeurjc/global_mart/controller/SPAController.java
+++ backend/src/main/java/es/codeurjc/global_mart/controller/SPAController.java
@@ -1,11 +1,11 @@
 package es.codeurjc.global_mart.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SPAController {
-    @RequestMapping({"/new", "/new/", "/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}"})
+    @GetMapping({"/new", "/new/", "/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}"})
     public String redirectAngularRoutes() {
         return "forward:/new/index.html";
     }
