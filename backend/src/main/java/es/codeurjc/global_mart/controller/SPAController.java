a021b0fd10a2b9069b7ebdd1a1cf2b6dd4e37c10
Alvaropin17 <alvaro17pc@gmail.com>
2025-04-28 01:05:35 +0200

    Fix spa controller

--- backend/src/main/java/es/codeurjc/global_mart/controller/SPAController.java
+++ backend/src/main/java/es/codeurjc/global_mart/controller/SPAController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class RouteController {
-    @RequestMapping("/new/**")
+public class SPAController {
+    @RequestMapping({"/new", "/new/", "/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}"})
     public String redirectAngularRoutes() {
         return "forward:/new/index.html";
     }
